c39542d168d3c8d48bec3e6aaf8df684d1104671
Ismayil Shirinov <sunqurlar@gmail.com>
2024-07-05 01:36:27 +0300

    Logout function added

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,23 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => {
+    const userString = window.localStorage.getItem('loggedBlogappUser')
+    if (userString) {
+      const user = JSON.parse(userString)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
     try {
       const user = await loginService.login({ username, password })
 
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
       setUser(user)
       setUsername('')
       setPassword('')
@@ -29,6 +40,12 @@ const App = () => {
       console.error(error);
     }
   }
+
+  const handleLogout = async (event) => {
+    setUser(null)
+    window.localStorage.removeItem('loggedBlogappUser')
+  }
+
   if (user === null) {
     return (
       <LoginForm
@@ -43,7 +60,7 @@ const App = () => {
     return (
       <div>
         <h2>blogs</h2>
-        <p>{user.name} logged in</p>
+        <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
